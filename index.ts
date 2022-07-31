45b09bb29cf94e4a55e9661a881557d5a5e2c687
Shubham Singh <shubhamsng7@gmail.com>
2022-07-31 15:52:58 +0530

    add add to cart route

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
-import { Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
+import cartRoutes from "./src/routes/cart.route";
+import client from "./src/routes/database";
 
-const express = require('express');
-const app = express();
+const app: Application = express();
 
+app.use(express.json());
+
+app.use(function(request: Request, response: Response, next: NextFunction) {
+    try {
+        const databaseState = client.currentStateOfDB();
+        console.log(JSON.stringify(databaseState, null, 2));
+        return next();
+    } catch (error: Error | any) {
+        console.error(error);
+        return response.status(500).json({status: false, message: error.message || 'Something went wrong' })
+    }
+})
+
+app.use("/cart", cartRoutes);
 
 app.listen(3000, () => console.log(`listening on port 3000`));
